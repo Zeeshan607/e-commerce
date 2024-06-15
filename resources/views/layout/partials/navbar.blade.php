8f6ef96143f52e463dfba0fd263340b70546e4f7
Muhammad Zeeshan <zeeshanawan1998@gmail.com>
2024-06-15 22:50:39 +0500

    added style.css file of theme which was missing from public folder, used blade directives and directory structure to saperate reuseable components like header and footer, fixed all public resource url of homepage , header and footer, [front end theme injected in project successfully, basic layout structure is made and now can be extended to develop further pages at client side ]

---
+++ resources/views/layout/partials/navbar.blade.php
@@ -0,0 +1,41 @@
+<!-- Begin Header Bottom Area -->
+<div class="header-bottom header-sticky d-none d-lg-block d-xl-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <!-- Begin Header Bottom Menu Area -->
+                <div class="hb-menu">
+                    <nav>
+                        <ul>
+                            <li class="">
+                                <a href="/">Home</a>
+                            </li>
+                            <li class="">
+                                <a href="/shop">Shop</a>
+                            </li>
+
+                            <li>
+                                <a href="about-us">About Us</a></li>
+                            <li>
+                                <a href="contact">Contact</a></li>
+{{--                            <li><a href="shop-left-sidebar.html">Smartwatch</a></li>--}}
+{{--                            <li><a href="shop-left-sidebar.html">Accessories</a></li>--}}
+                        </ul>
+                    </nav>
+                </div>
+                <!-- Header Bottom Menu Area End Here -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Header Bottom Area End Here -->
+<!-- Begin Mobile Menu Area -->
+<div class="mobile-menu-area d-lg-none d-xl-none col-12">
+    <div class="container">
+        <div class="row">
+            <div class="mobile-menu">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Mobile Menu Area End Here -->
