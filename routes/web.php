f1ecdcaa3cd64a2d96b7ecd7580006bff3cebcd9
Muhammad Zeeshan <zeeshanawan1998@gmail.com>
2024-06-17 23:29:44 +0500

    fixed merge conflict in routes/web.php file

--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,31 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
 
 
-Route::get("/shop", function (){
-    return view('shop');
-});
 
-<<<<<<< HEAD
-\Auth::routes();
 
-Route::get('/get-admin',function(){
-    $admin=\App\Models\Admin::all();
-   return $admin;
-});
 
-=======
-
+//user login routes
 Auth::routes();
->>>>>>> c918cc21c5e5d2ec3ac47688dd0f158902afd674
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
 //admin routes
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('/login', [App\Http\Controllers\Admin\Auth\AdminLoginController::class,"showLoginForm"])->name('login');
     Route::post('/login', [App\Http\Controllers\Admin\Auth\AdminLoginController::class,'adminLogin']);
     Route::post('/logout', [App\Http\Controllers\Admin\Auth\AdminLoginController::class,'adminLogout'])->name('logout')->middleware('auth:admin');
 });
+
+
+
+///client side public routes
+Route::get('/', function () {
+    return view('home');
+});
+Route::get("/shop", function (){
+    return view('shop');
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
